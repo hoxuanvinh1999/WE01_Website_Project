ffae232c60aaa31ef4ac1cb56fd63076d34ad061
Hồ Xuân Vinh <xuan-vinh.ho@etu.utc.fr>
2021-03-18 10:12:30 +0100

    Change page with new plan, upload js, change css

---
+++ decorationjs.js
@@ -0,0 +1,24 @@
+window.alert("Bienvenue sur notre page web du projet WE01")
+
+let groupmember = document.getElementById("groupmember")
+let members = document.getElementById("members")
+var check = false
+
+groupmember.onmouseover = function() {ShowInfor(members)}
+groupmember.onmouseout = function() {HideInfor(members)}
+groupmember.onclick= function(){ShowInforEver(members)}
+
+function ShowInfor(targer) {
+ if(check == false ){targer.style.display = 'block'}
+}
+
+function HideInfor(target) {
+ if(check == false){target.style.display = 'none'}
+}
+
+function ShowInforEver(targer){
+ if(check == false){
+    check = true
+    targer.style.display = 'non'
+ }
+}
